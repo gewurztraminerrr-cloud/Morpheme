0ad232f600c79896f6ebb5789a2d5fc8e73accc8
Jeff Babiak <jeffbabiak@Jeffs-MacBook-Air.local>
2026-05-19 17:30:18 -0500

    Temporarily increase Either/Or board format frequency weight to 70% for rapid diagnostics

--- spinner_set.py
+++ spinner_set.py
@@ -196,7 +196,7 @@ class SpinnerSet:
             
         result = random.choices(
             ['Normal', 'Checkerboard', 'Penalty', 'Mania', 'Either/Or', 'Bonus Letter', 'Valued Letters', 'Density', 'Rotating Letters'],
-            weights=[78, 8, 2, 2, 2, 2, 2, 2, 2]
+            weights=[14, 8, 2, 2, 70, 1, 1, 1, 1]
         )[0]
         
         if result == 'Mania':
