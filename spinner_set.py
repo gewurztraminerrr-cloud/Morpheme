24ad36b0432a31792ecb53a6b425395340446789
Jeff Babiak <jeffbabiak@Jeffs-MacBook-Air.local>
2026-05-19 19:26:05 -0500

    Restore standard board format weightings (70% Normal)

--- spinner_set.py
+++ spinner_set.py
@@ -196,7 +196,7 @@ class SpinnerSet:
             
         result = random.choices(
             ['Normal', 'Checkerboard', 'Penalty', 'Mania', 'Either/Or', 'Bonus Letter', 'Valued Letters', 'Density', 'Rotating Letters'],
-            weights=[1, 1, 1, 1, 95, 1, 0, 0, 0]
+            weights=[70, 10, 5, 5, 5, 5, 0, 0, 0]
         )[0]
         
         if result == 'Mania':
