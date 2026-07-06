704ddfef0e82c601f67a03a7e647e5681d0319d5
Jeff Babiak <jeffbabiak@Jeffs-MacBook-Air.local>
2026-07-06 13:59:17 -0500

    Add check_db_remote.py

---
+++ scratch/check_db_remote.py
@@ -0,0 +1,18 @@
+import sqlite3
+import json
+
+conn = sqlite3.connect('morpheme.db')
+cursor = conn.cursor()
+cursor.execute("SELECT DISTINCT param_key FROM pregenerated_boards;")
+keys = cursor.fetchall()
+dicts = set()
+for key in keys:
+    try:
+        data = json.loads(key[0])
+        dicts.add(data.get('dictionary'))
+    except Exception as e:
+        pass
+print("Unique dictionaries in remote pregenerated_boards:")
+for d in dicts:
+    print(d)
+conn.close()
