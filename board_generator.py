fa08a78f10f778da0ae4cc6391148971d5c1a41a
Jeff Babiak <jeffbabiak@Jeffs-MacBook-Air.local>
2026-07-18 20:20:31 -0500

    Deploy updates

--- board_generator.py
+++ board_generator.py
@@ -2502,14 +2502,9 @@ class BoardGenerator:
             
             if min_word_length >= 4:
                 weights = LETTER_FREQ_SUPER_DENSITY
-                # USER REQUEST: If we are forced to use Super Density frequencies in emergency mode,
-                # update the format to 'Density' so the Spinner Set doesn't abruptly change to a different
-                # format (like Bonus Letter) at the start of the round.
-                if "density" not in safe_format and safe_format in ["", "normal"]:
-                    board_format = "Density"
-                    safe_format = "density"
             else:
                 weights = LETTER_FREQ_EASY
+
             
             all_excluded = set()
             special_cells = []
